e1396f84d89223528a9478d4009e1b07cfd8bf8d
Nicole <nicolemariejones1@gmail.com>
2022-08-23 22:58:28 +0100

    fix up errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import type { FC, ReactNode } from 'react';
 import React, { useMemo } from 'react';
 import Wallet from './contexts/Wallet';
+import Demo from "./demo"
 
 export const App: FC = () => {
   return (
     <Wallet>
       <Content />
+      <Demo />
     </Wallet>
+
   );
 };
 
 const Content: FC = () => {
-  return <WalletMultiButton />;
+  return <div className="flex mx-auto"><WalletMultiButton /></div>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  content: [],
   theme: {
     extend: {},
   },
