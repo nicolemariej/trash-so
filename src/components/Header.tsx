b8a906aaf6ce51acb5ee8f88cce0f7d77d89c9f0
Nicole <nicolemariejones1@gmail.com>
2022-08-27 13:22:49 +0100

    style navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 import type { FC, ReactNode } from 'react';
 import React, { useMemo } from 'react';
 import logo from '../assets/logo.png'
+import { FaTwitter} from 'react-icons/fa';
+import { FaDiscord} from 'react-icons/fa';
 
 
 export const Header: FC = () => {
   return (
    <header>
-   <img src={logo} alt="Trash Logo" />
+    <nav className='flex justify-between mt-3'>
+   <img className='h-10 mt-2' src={logo} alt='Trash Logo' />
+  <span className='flex'>
+<div className='text-blue-500 mr-5 text-5xl'><FaTwitter /></div>
+<div className='text-blue-500 mr-20 text-5xl'><FaDiscord /></div>
+<span className='bg-black text-white px-9 py-2 rounded-full'> HA17..ORBZ</span>
+  </span>
+  </nav>
    </header>
 
   );
