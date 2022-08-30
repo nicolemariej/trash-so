4c2f05fd649f6fec507133ab6eafde9f8a92b6a8
Nicole <nicolemariejones1@gmail.com>
2022-08-30 22:17:46 +0100

    Implement responsive design

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -8,27 +8,28 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export const Description: FC = () => {
   return (
-   <div className='mt-36 ml-9 flex'>
-  <img src={token} alt='App preview' className='h-full max-w-[50%] mt-5' />
+   <div className='mt-20 lg:mt-36 ml-9 lg:flex'>
+  <img src={token} alt='App preview' className='invisible lg:visible h-0 lg:h-full max-w-[50%] mt-5' />
      <div className='ml-9'>
-       <div className=' mb-11 ml-9 mr-6 bg-gray-100 p-7 rounded-lg drop-shadow max-w-[60%]'>
+     <div className=' mb-11 md:ml-9 mr-12 md:mr-6 bg-gray-100 p-7 rounded-lg drop-shadow l:max-w-[60%]'>
         <h2 className='text-purple-600 text-5xl mb-6 font-medium '>Lorem Ipsum</h2>
     <div className='font-medium'>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
        </div>
        </div>
-       <div className=' mb-11 ml-9 mr-6 bg-gray-100 p-7 rounded-lg drop-shadow max-w-[60%]'>
+       <div className=' mb-11 md:ml-9 mr-12 md:mr-6 bg-gray-100 p-7 rounded-lg drop-shadow l:max-w-[60%]'>
         <h2 className='text-purple-600 text-5xl mb-6 font-medium '>Lorem Ipsum</h2>
     <div className='font-medium'>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
        </div>
        </div>
-       <div className=' mb-11 ml-9 mr-6 bg-gray-100 p-7 rounded-lg drop-shadow max-w-[60%]'>
+       <div className=' mb-11 md:ml-9 mr-12 md:mr-6 bg-gray-100 p-7 rounded-lg drop-shadow l:max-w-[60%]'>
         <h2 className='text-purple-600 text-5xl mb-6 font-medium '>Lorem Ipsum</h2>
     <div className='font-medium'>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
        </div>
        </div>
+    
      </div>
 </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,16 +9,17 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export const Home: FC = () => {
   return (
-   <div className='flex'>
-    <div className='ml-36'>
- <img src={fish} alt='Fish' className='h-48 my-9' />
- <p className='text-8xl mb-14 mt-9 leading-tight'>Earn Solana for your trash.</p>
+   <div className='mx-16 md:ml-7 md:flex'>
+    <div className='ml-6 lg:ml-36'>
+ <img src={fish} alt='Fish' className='h-20 xl:h-48 my-9 ' />
+ <h1 className='text-4xl lg:text-6xl xl:text-8xl mb-14 mt-9 leading-tight'>Earn Solana for your trash.</h1>
  <div className='max-w-xs flex drop-shadow-xl'>
  <Content />
  </div>
    </div>
-   <img src={home} alt='App display' className='h-full max-w-[50%] mt-10' />
+   <img src={home} alt='App display' className='invisible h-0 md:visible md:h-full max-w-[50%] mt-10' />
 </div>
+
   );
 };
 
