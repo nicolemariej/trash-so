5c0f5026a45850fd2951efd4403696d3007e647a
Nicole <nicolemariejones1@gmail.com>
2022-09-12 20:46:06 +0100

    Add moralis dapp into index.tsx

--- src/components/Tokens.tsx
+++ src/components/Tokens.tsx
@@ -5,15 +5,22 @@ import { TokenCard } from './TokenCard';
 import fish from '../assets/fish.png';
 import dummy from './data';
 import { useWallet } from '@solana/wallet-adapter-react';
-
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { useMoralis, useMoralisSolanaApi, useMoralisSolanaCall } from 'react-moralis';
+import { useMoralis, useMoralisSolanaApi, useMoralisSolanaCall, useMoralisWeb3Api } from 'react-moralis';
 
 export const Tokens: FC = () => {
   const api = 'HLsfmH5MxvBeZoRo9FaPgTROl4aBIRXORCD36725DQcscog8gd42sIg44Lod5m8B';
   const { publicKey } = useWallet();
-  const serverUrl = 'https://a6xyqqo4hpy0.usemoralis.com:2053/server';
-  const appID = 'ZdHzvhari1ff954lOq6lpTruUGnnEodJXmL1cebI';
+
+  const { account } = useMoralisSolanaApi();
+  // get devnet SPL NFT metadata for a given address
+  const options = {
+    network: 'mainnet',
+    address: 'HA17Nu9w2wNoCKVH9PizMbJKvBDmZrPwVkpogWc5oRBz',
+  };
+  const { nft } = useMoralisSolanaApi();
+
+  const { fetch, data, isLoading } = useMoralisSolanaCall(nft.getNFTMetadata, options);
 
   if (publicKey) {
     const cards = dummy.map((item) => {
@@ -22,8 +29,8 @@ export const Tokens: FC = () => {
     return (
       <div>
         <div className="font-bold flex justify-center my-7">
-          {console.log(publicKey.toString())}Please select which NFT’s you would like to burn, you can unselect them
-          before finalising the burn.{' '}
+          {console.log(publicKey.toString())}Please select which NFT’efrwes you would like to burn, you can unselect
+          them before finalising the burn. {console.log(nft.toString())}
         </div>
         <div className="flex m-16 justify-center grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 ">
           {cards}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import "./index.css"
-import {BrowserRouter} from  'react-router-dom'
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from 'react-moralis';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MoralisProvider
+      serverUrl="https://a6xyqqo4hpy0.usemoralis.com:2053/server"
+      appId="ZdHzvhari1ff954lOq6lpTruUGnnEodJXmL1cebI"
+    >
+      <App />
+    </MoralisProvider>
   </BrowserRouter>,
   document.getElementById('app')
 );
